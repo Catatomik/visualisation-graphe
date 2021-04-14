fad4e86c6259a759ca0894879b9d37b9ef404e2f
Catatomik <cata@catadev.ga>
2021-04-14 03:08:46 +0200

    :sparkles: Ajout d'une classe Graphe Pondéré

---
+++ utils/Graphe_pondere.js
@@ -0,0 +1,34 @@
+class GraphePondere extends Graphe {
+
+    constructor() {
+        super()
+        this.poids = {}
+    }
+
+    /**
+     * @description Ajoute un arc entre deux sommets s1 et s2
+     * @param {String} s1 Un sommet du graphe
+     * @param {String} s2 Un sommet du graphe
+     * @param {Number} p Le poids de cet arc
+     * @returns {Graphe}
+     */
+    ajouter_arc(s1, s2, p) {
+        super.ajouter_arc(s1, s2)
+        this.poids[`${s1}-${s2}`] = p
+        return this
+    }
+
+    /**
+     * @description Ajouter une arête entre deux sommets s1 et s2
+     * @param {String} s1 Un sommet du graphe
+     * @param {String} s2 Un sommet du graphe
+     * @param {Number} p Le poids de cet arc
+     * @returns {Graphe}
+     */
+     ajouter_arete(s1, s2, p) {
+        this.ajouter_arc(s1, s2, p)
+        this.ajouter_arc(s2, s1, p)
+        return this
+    }
+
+}
